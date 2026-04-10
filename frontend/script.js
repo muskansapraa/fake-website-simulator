664e01c36eb44ae00b1e2fff0db5cbd5e0ed310d
Muskan Sapra <muskansapra2006@gmail.com>
2026-04-10 19:31:18 +0530

    Updated UI to Instagram Style

---
+++ frontend/script.js
@@ -0,0 +1,22 @@
+function startTest() {
+    window.location.href = "login.html";
+}
+
+function handleLogin(event) {
+    event.preventDefault();
+    // Store that user entered credentials
+    localStorage.setItem("clickedLogin", "true");
+
+    alert("Verifying your account...");
+
+    setTimeout(() => {
+        window.location.href = "result.html";
+    }, 2000);
+
+
+    // Simulate phishing success
+    window.location.href = "result.html";
+}
+function goHome() {
+    window.location.href = "index.html";
+}
